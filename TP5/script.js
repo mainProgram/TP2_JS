64205261800322c75e6afe56c619aa9aad52ff85
mainProgram <fazeyfadiop@gmail.com>
2022-03-25 10:19:07 +0000

    TP5 search icon

--- TP5/script.js
+++ TP5/script.js
@@ -4,6 +4,7 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const MAIN = document.querySelector("main")
 const LOADER = document.querySelector(".loader")
+const SEARCH_ICON = document.querySelector("#search")
 const SEARCH = document.querySelector("input[type='search']")
 const TOP_ARROW = document.querySelector("a[href='#header']")
 
@@ -19,54 +20,59 @@ function notFound(){
     MAIN.appendChild(h1)
 }
 
-function addFilms(data){
-    fetch(data).then(response => 
-        response.json().then(data => {
-            for (let film of data.results) {
-                    
-                original_title = film.original_title
-                vote_average = film.vote_average
-                poster_path = film.poster_path
-                overview = film.overview
-        
-                section = document.createElement("section")
-                section.setAttribute("class", "film")
-        
-                img = document.createElement("img")
-                img.setAttribute("src", IMG_PATH + poster_path)
-        
-                div = document.createElement("div")
-                div.setAttribute("class", "film-details")
-                h4 = document.createElement("h4")
-                h4.innerHTML = original_title
-                span = document.createElement("span")
-                span.innerHTML = vote_average
-                if(vote_average < 5)
-                    span.setAttribute("class", "red")
-                else if(vote_average >= 5 && vote_average < 8)
-                    span.setAttribute("class", "orange")
-                else
-                    span.setAttribute("class", "green")
-                div.appendChild(h4)
-                div.appendChild(span)
-        
-                divOverview = document.createElement("div")
-                divOverview.setAttribute("class", "overview")
-                h3 = document.createElement("h3")
-                h3.innerHTML = original_title
-                p = document.createElement("p")
-                p.innerHTML = overview
-                divOverview.appendChild(h3)
-                divOverview.appendChild(p)
-        
-                section.appendChild(img)
-                section.appendChild(div)
-                section.appendChild(divOverview)
-        
-                MAIN.appendChild(section)
-            }
-        })
-    )
+async function getDatas(url){
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+function addFilms(url){
+    const datas = getDatas(url)
+    datas.then(data => {
+        for (let film of data.results) {
+                
+            original_title = film.original_title
+            vote_average = film.vote_average
+            poster_path = film.poster_path
+            overview = film.overview
+    
+            section = document.createElement("section")
+            section.setAttribute("class", "film")
+    
+            img = document.createElement("img")
+            img.setAttribute("src", IMG_PATH + poster_path)
+    
+            div = document.createElement("div")
+            div.setAttribute("class", "film-details")
+            h4 = document.createElement("h4")
+            h4.innerHTML = original_title
+            span = document.createElement("span")
+            span.innerHTML = vote_average
+            if(vote_average < 5)
+                span.setAttribute("class", "red")
+            else if(vote_average >= 5 && vote_average < 8)
+                span.setAttribute("class", "orange")
+            else
+                span.setAttribute("class", "green")
+            div.appendChild(h4)
+            div.appendChild(span)
+    
+            divOverview = document.createElement("div")
+            divOverview.setAttribute("class", "overview")
+            h3 = document.createElement("h3")
+            h3.innerHTML = original_title
+            p = document.createElement("p")
+            p.innerHTML = overview
+            divOverview.appendChild(h3)
+            divOverview.appendChild(p)
+    
+            section.appendChild(img)
+            section.appendChild(div)
+            section.appendChild(divOverview)
+    
+            MAIN.appendChild(section)
+        }
+    })
 }
 
 const scroll = function scroll(){
@@ -91,29 +97,52 @@ const scroll = function scroll(){
         TOP_ARROW.firstChild.classList.remove("show")
 
 }
+
+function firstPage(){
+    MAIN.innerHTML = ""
+    addFilms(API_URL+1)
+    page = 1
+    window.addEventListener("scroll", scroll)
+}
+
+function search(value){
+    MAIN.innerHTML = ""
+    addFilms(value)
+    window.removeEventListener("scroll", scroll)
+    LOADER.classList.remove("show")
+}
 // ------------------------------------------------------------------------------------EVENTS
 let page = 1
 window.addEventListener("scroll", scroll)
 
 SEARCH.addEventListener("input", () => {
 
-    if(SEARCH.value.length != "" && SEARCH.value.length > 2 ){
+    if(SEARCH.value.length != "" && SEARCH.value.length > 3 ){
         fetch(SEARCH_API + SEARCH.value).then(response => response.json().then(data => {
             if(data.results.length > 0){
-                MAIN.innerHTML = ""
-                addFilms(SEARCH_API + SEARCH.value)
-                window.removeEventListener("scroll", scroll)
-                LOADER.classList.remove("show")
+                search(SEARCH_API + SEARCH.value)
             }       
             else
                 notFound()
         }))
     }
     else{
-        MAIN.innerHTML = ""
-        addFilms(API_URL+1)
-        page = 1
-        window.addEventListener("scroll", scroll)
+        firstPage()
+    }
+})
+
+SEARCH_ICON.addEventListener("click", ()=>{
+    if(SEARCH.value.length != "" ){
+        fetch(SEARCH_API + SEARCH.value).then(response => response.json().then(data => {
+            if(data.results.length > 0){
+                search(SEARCH_API + SEARCH.value)
+            }       
+            else
+                notFound()
+        }))
+    }
+    else{
+        firstPage()
     }
 })
 
