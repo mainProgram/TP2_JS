ec05f1e598ff003f44984730de1643001b866bf2
mainProgram <fazeyfadiop@gmail.com>
2022-04-02 18:06:32 +0000

    g

---
+++ PROJECT/variables.js
@@ -0,0 +1,24 @@
+//--------------------------------------------------------------------------------------------------DECLARATIONS
+const HEADER = document.querySelector("header")
+const SECTION = document.querySelector("section")
+const CLOSE_ICON_HEADER = document.querySelector(".close")
+const ARROW_DOWN = document.querySelector(".arrow-down")
+const ADD_TASK = document.querySelector(".add-task")
+const ADD_COLUMN = document.querySelector(".add-column")
+const CLOSE_ICON_MODAL = document.getElementById("close");
+const MODAL = document.getElementById("modal");
+const TASK_DATE = document.querySelector("#date")
+const TASK_DESCRIPTION = document.querySelector("#task")
+const TASK_BUTTON_EDIT = document.querySelector("#edit")
+const TASK_BUTTON_SUBMIT = document.querySelector("#submit")
+const TASK_START_TIME = document.querySelector("#start-time")
+const TASK_ENDING_TIME = document.querySelector("#ending-time")
+const TRASH_ICON= document.getElementById("trash")
+const TRASH_BODY= document.querySelector(".trash")
+const ALERT = document.querySelector(".alert")
+const BTN_CLOSE_ALERT = document.querySelector(".close-alert")
+const SAVE_STATE = document.querySelector(".save-state")
+const LIST_BODY = document.querySelector(".list-states-body")
+const LIST_STATES_BTN = document.querySelector(".list-states")
+
+const TASK_INFOS = [TASK_DATE, TASK_DESCRIPTION, TASK_START_TIME, TASK_ENDING_TIME]
